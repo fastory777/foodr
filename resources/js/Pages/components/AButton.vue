742f3992ce86be5f80c39bdd48129194807a6655
Fredrik Krosktad <fredrik@intrinsic.no>
2025-07-12 15:54:34 +0200

    Changed red color preset of AButton to be more consistent with already utilized tailwind colors used for validation

--- resources/js/Pages/components/AButton.vue
+++ resources/js/Pages/components/AButton.vue
@@ -35,7 +35,7 @@ export default {
             defaultClasses: 'inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none',
             colorOptions: {
                 blue: 'bg-blue-700 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:bg-blue-800',
-                red: 'bg-red-700 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 hover:bg-red-800',
+                red: 'bg-red-500 focus:ring-red-100 dark:bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600 hover:bg-red-600',
                 yellow: 'bg-yellow-700 focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 hover:bg-yellow-800',
                 green: 'bg-green-700 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 hover:bg-green-800',
                 black: 'bg-black focus:ring-black dark:bg-black dark:hover:bg-black dark:focus:ring-grey-700 hover:bg-grey-800',
